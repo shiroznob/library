a4e1372954a4fb4105c17966533c44c9f0b3d5ec
shiroznob <bayu.satyalaksana@gmail.com>
2021-06-26 17:35:03 +0800

    lanjutan

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $koneksi = mysqli_connect($host, $user, $pass);
+
+    if(!$koneksi) {
+        die ("Gagal menyambungkan koneksi: ".mysqli_errno($koneksi)." - ".mysqli_error($koneksi));
+    }
+
+    //memilih database library
+    $result = mysqli_select_db($koneksi, "library");
+    if(!$result){
+        die ("Gagal memilih database: ".mysqli_errno($koneksi)." - ".mysqli_error($koneksi));
+    }
+?>
